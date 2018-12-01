b84c6333dfbe8fceea99eb9984c08b0205686acf
Saad Ahmad <deburgers123@gmail.com>
2018-12-02 02:36:07 +0500

    init about us page

---
+++ js/data.js
@@ -0,0 +1,70 @@
+/* About Us tile data -- START -- */
+const personalInfo = {
+    image : "images/photo-about.jpg",
+    intro : `   
+                I'm a Freelance Web Designer & Developer based in Madrid, Spain. 
+                qI have serious passion for UI effects, animations and creating intuitive, 
+                with over a decade of experience.
+            `,
+    firstName : "Saad",
+    lastName : "Ahmad",
+    DOB : "March 1st, 1998",
+    nationality : "Pakistan",
+    freelance : "Available",
+    phone : "(+92)332 6363603",
+    address : "Punjab, Pakistan",
+    email : "saadahmadkwi123@gmail.com",
+    spokeLanguage : "English - Urdu",
+    skype : "Coming Soon",
+}
+
+const expList = [
+    {
+        job : "Web Designer",
+        timeSpan : "2016 - Present",
+        text : "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus pretium orci sit amet mi ullamcorper"
+    },
+    {
+        job : "Web Designer",
+        timeSpan : "2016 - Present",
+        text : "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus pretium orci sit amet mi ullamcorper"
+    },
+];
+
+
+const educationList = [
+    {
+        job : "Bachelors in Electrical Engineering - NUST, Islamabad",
+        timeSpan : "2016 - Present",
+        text : "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus pretium orci sit amet mi ullamcorper"
+    },
+    {
+        job : "Web Designer",
+        timeSpan : "2016 - Present",
+        text : "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus pretium orci sit amet mi ullamcorper"
+    },
+];
+
+const skillsList = [
+    {
+        skill : "HTML",
+        stars : 4
+    },{
+        skill : "Javascript",
+        stars : 5
+    },{
+        skill : "HTML",
+        stars : 4
+    },{
+        skill : "HTML",
+        stars : 4
+    },{
+        skill : "HTML",
+        stars : 4
+    },{
+        skill : "HTML",
+        stars : 2
+    },
+]
+
+/* About Us tile data -- END -- */
--- js/reactApp.js
+++ js/reactApp.js
@@ -1,13 +1,3 @@
-function PreLoader(props){
-    return (
-        <div id="loader-wrapper">
-            <div id="loader"></div>
-            <div className="loader-section section-left"></div>
-            <div className="loader-section section-right"></div>
-        </div>
-    );
-}
-
 function MainImageTile(props){
     const personalAttributes = props.personalAttributes;
     return (
@@ -52,11 +42,245 @@ function TileTitle(props){
     );
 }
 
+function TileContentTitle(props){
+    return(
+        <div className="container page-title center-align">
+            <h2 className="center-align">
+                <span data-hover={props.titleText1}>{props.titleText1}</span>
+                <span data-hover={props.titleText2}>{props.titleText2}</span>
+            </h2>
+            <span className="title-bg">{props.backgroundTitle}</span>
+        </div>
+    );    
+}
+
+function TileContentCloseBtn(props){
+    return (
+        <img alt="close" src="images/close-button.png" className="bl-icon-close"></img>
+    );
+}
+
+function TileContentDivider(props){
+    return (
+        <div className="divider center-align">
+            <span className="outer-line"></span>
+            <span className={"fa " + props.icon} aria-hidden="true"></span>
+            <span className="outer-line"></span>
+        </div>
+    );
+}
+
+function AboutTileInfo(props){
+    return (
+        <div className="row">
+            <div className="col s12 m5 l4 xl3 profile-picture">
+				<img src={props.image} className="responsive-img my-picture" alt="My Photo"/>
+            </div>
+            <div className="col s12 m7 l8 xl9 personal-info">
+                <h6 className="uppercase"><i className="fa fa-user"></i> Personal Informations</h6>
+				<div className="col m12 l7 xl7 p-none">
+                    <p className="second-font">
+                        {props.personalInfo}
+                    </p>
+                </div>
+                <div className="col s12 m12 l6 p-none">
+                    <ul className="second-font list-1">
+                        <li><span className="font-weight-600">First Name: </span>{props.firstName}</li>
+                        <li><span className="font-weight-600">Last Name: </span>{props.lastName}</li>
+                        <li><span className="font-weight-600">Date of birth: </span>{props.DOB}</li>
+                        <li><span className="font-weight-600">Nationality: </span>{props.nationality}</li>
+						<li><span className="font-weight-600">Freelance: </span>{props.freelance}</li>
+                    </ul>
+                </div>
+                <div className="col s12 m12 l6 p-none">
+                    <ul className="second-font list-2">
+                        <li><span className="font-weight-600">Phone: </span>{props.phone}</li>
+                        <li><span className="font-weight-600">Address: </span>{props.address}</li>
+                        <li><span className="font-weight-600">Email: </span>{props.email}</li>
+                        <li><span className="font-weight-600">Spoken Langages: </span>{props.spokeLanguage}</li>
+						<li><span className="font-weight-600">Skype: </span>{props.skype}</li>
+                    </ul>
+                </div>
+                <a href="#" className="col s12 m12 l4 xl4 waves-effect waves-light btn font-weight-500">
+					Download Resume <i className="fa fa-file-pdf-o"></i>
+				</a>
+				{/*<a href="blog-dark.html" className="col s12 m12 l4 xl4 btn btn-blog font-weight-500">
+					My Blog <i className="fa fa-edit"></i>
+				</a>*/}
+            </div>
+        </div>
+    );
+}
+
+function AboutTileResumeMenu(props){
+    return(
+            <div className="resume-list col l4">
+                <div className="resume-list-item is-active" data-index="0" id="resume-list-item-0">
+                    <div className="resume-list-item-inner">
+                        <h6 className="resume-list-item-title uppercase"><i className="fa fa-briefcase"></i> Experience</h6>
+                    </div>
+                </div>
+                <div className="resume-list-item" data-index="1" id="resume-list-item-1">
+                    <div className="resume-list-item-inner">
+                        <h6 className="resume-list-item-title uppercase"><i className="fa fa-graduation-cap"></i> Education</h6>
+                    </div>
+                </div>
+                <div className="resume-list-item" data-index="2" id="resume-list-item-2">
+                    <div className="resume-list-item-inner">
+                        <h6 className="resume-list-item-title uppercase"><i className="fa fa-star"></i> Skills</h6>
+                    </div>
+                </div>
+            </div>        
+    );
+}
+
+function ExpItem(props){
+    // Strictly tied to the AboutTileResumeExperienceCard()
+    return (
+        <div>
+            <div className="resume-content">
+                <h6 className="uppercase"><span>{props.job}</span></h6>
+                <span className="date"><i className="fa fa-calendar-o"></i>&nbsp;{props.timeSpan}</span>
+                <p>{props.text}</p>
+            </div>
+            <span style={{height:"2px", width:"70px", marginBottom:"24px", display:"block", backgroundColor:"yellow"}}></span>
+        </div>
+    );
+}
+
+function AboutTileResumeExperienceCard(props){
+    const expList = props.experienceList;
+    return(
+        <div className={"resume-card resume-card-" + props.dataIndex} data-index={props.dataIndex}>
+            <div className="resume-card-header">
+                <div className="resume-card-name">
+                    <i className={"fa " + props.titleIcon}></i>
+                    &nbsp;{props.title}
+                </div>
+            </div>
+            <div className="resume-card-body experience">
+                <div className="resume-card-body-container second-font">
+                    {
+                        expList.map((exp, idx) => 
+                            <ExpItem 
+                                key={idx.toString()+"_kkk"} 
+                                job={exp.job}
+                                timeSpan={exp.timeSpan}
+                                text={exp.text}
+                            />
+                        )
+                    }
+                </div>
+            </div>
+        </div>
+    );
+}
+
+function SkillItem(props){
+    var skillStars = [];
+    for (var i = 0; i < props.stars; i++){
+        skillStars.push(<i className="fa fa-star" style={{color:"#ffb400"}}>&nbsp;</i>);
+    }
+    return (
+        <div className="resume-content">
+		    <h6 className="uppercase">{props.skill}</h6>
+			<p>
+                {skillStars}
+            </p>
+        </div>
+    );
+}
+
+function AboutTileSkillsCard(props){
+    var skillsList = props.skillsList;
+    var half_length = Math.ceil(skillsList.length / 2);
+    var skillsList1 = skillsList.splice(0,half_length);
+    var skillsList2 = skillsList;
+    return(
+        <div className={"resume-card resume-card-" + props.dataIndex} data-index={props.dataIndex}>
+            <div className="resume-card-header">
+                <div className="resume-card-name">
+                    <i className={"fa " + props.titleIcon}></i>
+                    &nbsp;{props.title}
+                </div>
+            </div>
+            <div className="resume-card-body experience">
+                <div className="resume-card-body-container second-font">
+                    <div className="row">
+                        <div className="col s6">
+                            {skillsList1.map((skill, idx) => <SkillItem skill={skill.skill} stars={skill.stars}/>)}
+                        </div>
+                        <div className="col s6">
+                            {skillsList2.map((skill, idx) => <SkillItem skill={skill.skill} stars={skill.stars}/>)}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+function AboutTileResume(props){
+    return(
+        <div className="resume-container">
+            <div className="container">
+                <div className="valign-wrapper row">
+                    <AboutTileResumeMenu />
+                    <div className="col s12 m12 l8 resume-cards-container">
+                        <div className="resume-cards">
+                            <AboutTileResumeExperienceCard
+                                dataIndex="0"
+                                titleIcon="fa-briefcase" 
+                                title="Experience"
+                                experienceList={expList}
+                            />
+                            <AboutTileResumeExperienceCard
+                                dataIndex="1"
+                                titleIcon="fa-graduation-cap"
+                                title="Education"
+                                experienceList={educationList}
+                            />
+                            <AboutTileSkillsCard 
+                                dataIndex="2"
+                                titleIcon="fa-star"
+                                title="Skills"
+                                skillsList = {skillsList}
+                            />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
 class AboutTile extends React.Component{
     render(){
         return (
         <section>
             <TileTitle backgroundTitle="RESUME" titleText1="About&nbsp;" titleText2="ME"/>
+            <div className="bl-content">
+                <TileContentTitle backgroundTitle="RESUME" titleText1="About&nbsp;" titleText2="ME"/>
+                <div className="container infos">
+                    <TileContentDivider icon="fa-vcard"/>
+                    <AboutTileInfo image={personalInfo.image}
+                                personalInfo={personalInfo.intro}
+                                firstName = {personalInfo.firstName}
+                                lastName = {personalInfo.lastName}
+                                DOB = {personalInfo.DOB}
+                                nationality = {personalInfo.nationality}
+                                freelance = {personalInfo.freelance}
+                                phone = {personalInfo.phone}
+                                address = {personalInfo.address}
+                                email = {personalInfo.email}
+                                spokeLanguage = {personalInfo.spokenLanguage}
+                                skype = {personalInfo.skype}
+                    />
+                </div>
+                <AboutTileResume />
+            </div>
+            
+            <TileContentCloseBtn />
         </section>
         );
     }
@@ -67,6 +291,10 @@ class PortfolioTile extends React.Component{
         return(
             <section>
                 <TileTitle backgroundTitle="WORKS" titleText1="MY&nbsp;" titleText2="PORTFOLIO"/>
+                <div className="bl-content">
+                    <TileContentTitle backgroundTitle="WORKS" titleText1="MY&nbsp;" titleText2="PORTFOLIO"/>
+                </div>
+                <TileContentCloseBtn />
             </section>
         );
     }
@@ -77,6 +305,10 @@ class ContactTile extends React.Component{
         return(
             <section>
                 <TileTitle backgroundTitle="CONTACT" titleText1="GET&nbsp;" titleText2="IN TOUCH"/>
+                <div className="bl-content">
+                    <TileContentTitle backgroundTitle="CONTACT" titleText1="GET&nbsp;" titleText2="IN TOUCH"/>
+                </div>
+                <TileContentCloseBtn />
             </section>
         );
     }
@@ -92,8 +324,7 @@ class ContactTile extends React.Component{
 class MainContentPage extends React.Component{
     render(){
         return (
-            <div className="wrapper">
-                <div id="bl-main" className="bl-main">
+            <div id="bl-main" className="bl-main">
                     <MainImageTile name="Saad Ahmad" 
                                     personalAttributes={["an Engineer", "a Problem Solver", "a Perfectionist"]}
                                     image = "images/main-photo.jpg"
@@ -101,7 +332,6 @@ class MainContentPage extends React.Component{
                     <AboutTile/>
                     <PortfolioTile/>
                     <ContactTile />
-                </div>
             </div>
         );
     }
@@ -111,7 +341,6 @@ function makeReactApp(){
     // This function contains all the markup
     // which is to be interpreted by babel.
     const markup = [
-        (<PreLoader />),
         (<MainContentPage />),
     ]
         
